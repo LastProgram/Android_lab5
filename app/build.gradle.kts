87f5a6a2c6bac7042d412012802166b29730232e
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-08 01:48:01 +0700

    create MainActivity and add seekBar element behaviour

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.android_lab5"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.android_lab5"
--- app/src/main/java/com/example/android_lab5/MainActivity.kt
+++ app/src/main/java/com/example/android_lab5/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.android_lab5
 
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var seekBar: SeekBar
+    private lateinit var progresTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        seekBar = findViewById<SeekBar>(R.id.seekBar)
+        progresTextView = findViewById<TextView>(R.id.progresTextView)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progresTextView.text = "${progress} %"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
 }
